8fd052e29bd2d39808821b36f11458d5965f463a
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 15:59:45 +0200

    Small fixes to Clan War commands

--- MasterBot.Service/Modules/ClanWarsModule.cs
+++ MasterBot.Service/Modules/ClanWarsModule.cs
@@ -11,9 +11,11 @@ namespace MasterBot.Service.Modules
         [Summary("Posts an explanation about free wins in Clan War.")]
         public async Task FreeWin()
         {
-            var channel = Context.Channel;
-            await channel.SendMessageAsync(@"You can only get a free win if all of these requirements are satisfied:
-    1. No other player (or team) from your clan got a game
+            await ReplyAsync(@"Wait Private you don't know what a free win is and how to get it?
+Then give me your ATTENTION and listen up while i explain.
+
+You can only get a free win if all of these requirements are satisfied:
+    1. No other player (or team) from your clan got a game on this template
     2. No opponents could be found for you (or your team)");
         }
 
@@ -21,8 +23,7 @@ namespace MasterBot.Service.Modules
         [Summary("Posts an explanation about swarming templates in Clan War.")]
         public async Task Swarm()
         {
-            var channel = Context.Channel;
-            await channel.SendMessageAsync(@"Swarming happens when multiple Masters join the same template.
+            await ReplyAsync(@"Swarming happens when multiple Masters join the same template.
 It has several benefits:
     1. It ensures that, if some M'Hunters are queued, we're not just filtering the hardest opponents from them
     2. It makes us get direct matchups against M'Hunters
