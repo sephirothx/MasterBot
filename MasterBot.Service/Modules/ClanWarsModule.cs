5e57357f79dfc2601540f9c8cc43b4c71638c90c
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 15:37:02 +0200

    Added !swarm command. Closes #2

--- MasterBot.Service/Modules/ClanWarsModule.cs
+++ MasterBot.Service/Modules/ClanWarsModule.cs
@@ -16,5 +16,17 @@ namespace MasterBot.Service.Modules
     1. No other player (or team) from your clan got a game
     2. No opponents could be found for you (or your team)");
         }
+
+        [Command("swarm")]
+        [Summary("Posts an explanation about swarming templates in Clan War.")]
+        public async Task Swarm()
+        {
+            var channel = Context.Channel;
+            await channel.SendMessageAsync(@"Swarming happens when multiple Masters join the same template.
+It has several benefits:
+    1. It ensures that, if some M'Hunters are queued, we're not just filtering the hardest opponents from them
+    2. It makes us get direct matchups against M'Hunters
+    3. It gives us a chance of getting 'easier' opponents on average");
+        }
     }
 }
