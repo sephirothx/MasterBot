4a03424a1079c240934233811ef4f6cce3388575
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 15:30:32 +0200

    Added !freewin command. Closes #1

--- MasterBot.Service/Modules/ClanWarsModule.cs
+++ MasterBot.Service/Modules/ClanWarsModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using System.Threading.Tasks;
+using Discord.Commands;
 
 namespace MasterBot.Service.Modules
 {
@@ -6,6 +7,14 @@ namespace MasterBot.Service.Modules
     [Summary("Clan Wars specific commands")]
     public class ClanWarsModule : ModuleBase<SocketCommandContext>
     {
-        
+        [Command("freewin"), Alias("fw")]
+        [Summary("Posts an explanation about free wins in Clan War.")]
+        public async Task FreeWin()
+        {
+            var channel = Context.Channel;
+            await channel.SendMessageAsync(@"You can only get a free win if all of these requirements are satisfied:
+    1. No other player (or team) from your clan got a game
+    2. No opponents could be found for you (or your team)");
+        }
     }
 }
