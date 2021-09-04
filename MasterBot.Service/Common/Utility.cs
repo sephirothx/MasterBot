305bf2532cc5b23f3cf28bad4f8fb7ac2712936e
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 16:57:01 +0200

    Added Utility class

---
+++ MasterBot.Service/Common/Utility.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Microsoft.Extensions.Logging;
+
+namespace MasterBot.Service.Common
+{
+    public static class Utility
+    {
+        public static LogLevel GetLogLevel(LogSeverity severity)
+        {
+            return severity switch
+            {
+                LogSeverity.Verbose  => LogLevel.Trace,
+                LogSeverity.Debug    => LogLevel.Debug,
+                LogSeverity.Info     => LogLevel.Information,
+                LogSeverity.Warning  => LogLevel.Warning,
+                LogSeverity.Error    => LogLevel.Error,
+                LogSeverity.Critical => LogLevel.Critical,
+                _                    => LogLevel.None
+            };
+        }
+    }
+}
