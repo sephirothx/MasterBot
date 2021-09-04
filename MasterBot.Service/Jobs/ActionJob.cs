24c7ae55ce069ad12fe970974035ea4a45694c7f
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 17:28:14 +0200

    Added ActionJob class

---
+++ MasterBot.Service/Jobs/ActionJob.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace MasterBot.Service.Jobs
+{
+    public class ActionJob : IJob
+    {
+        public static JobBuilder Create(Action action)
+        {
+            return JobBuilder.Create<ActionJob>()
+                             .SetJobData(new JobDataMap
+                              {
+                                  { "action", action }
+                              });
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            if (context.MergedJobDataMap["action"] is Action action)
+            {
+                action();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
