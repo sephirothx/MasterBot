7fb3946f1eb9f6fe028bea43863de679dc6134f1
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-04 15:11:21 +0200

    Added admin command !say

---
+++ MasterBot.Service/Modules/AdminModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace MasterBot.Service.Modules
+{
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Makes the bot say something.")]
+        public async Task Say([Remainder] string text)
+        {
+            var message = Context.Message;
+            await message.DeleteAsync();
+
+            await ReplyAsync(text);
+        }
+
+        [Command("say")]
+        [Summary("Makes the bot say something in a specific channel.")]
+        public async Task Say(IMessageChannel channel, [Remainder] string text)
+        {
+            var message = Context.Message;
+            await message.DeleteAsync();
+
+            await channel.SendMessageAsync(text);
+        }
+    }
+}
