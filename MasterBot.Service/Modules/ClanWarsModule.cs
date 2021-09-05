6e2be56a36f2f6819539b7cc6583bec3bac95bc8
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-05 20:17:42 +0200

    Added !timeslot and !timeslot diff commands

--- MasterBot.Service/Modules/ClanWarsModule.cs
+++ MasterBot.Service/Modules/ClanWarsModule.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord.Commands;
+using MasterBot.Service.Common;
 
 namespace MasterBot.Service.Modules
 {
@@ -7,6 +9,13 @@ namespace MasterBot.Service.Modules
     [Summary("Clan Wars specific commands")]
     public class ClanWarsModule : ModuleBase<SocketCommandContext>
     {
+        private readonly Utility _utility;
+
+        public ClanWarsModule(Utility utility)
+        {
+            _utility = utility;
+        }
+
         [Command("freewin"), Alias("fw")]
         [Summary("Posts an explanation about free wins in Clan War.")]
         public async Task FreeWin()
@@ -28,6 +37,24 @@ It has several benefits:
     1. It ensures that, if some M'Hunters are queued, we're not just filtering the hardest opponents from them
     2. It makes us get direct matchups against M'Hunters
     3. It gives us a chance of getting 'easier' opponents on average");
+        }
+
+        [Command("timeslot"), Alias("lastslot", "ts", "ls")]
+        [Summary("Posts a link to the latest timeslot.")]
+        public async Task Timeslot()
+        {
+            await ReplyAsync("https://www.warzone.com/Clans/War"  +
+                             $"?ID={_utility.GetCurrentSeason()}" +
+                             $"&Timeslot={_utility.GetLastTimeslotNumber()}");
+        }
+
+        [Command("timeslot"), Alias("ts")]
+        [Summary("Posts a link to a previous timeslot.")]
+        public async Task Timeslot(int diff)
+        {
+            await ReplyAsync("https://www.warzone.com/Clans/War"  +
+                             $"?ID={_utility.GetCurrentSeason()}" +
+                             $"&Timeslot={_utility.GetLastTimeslotNumber() - Math.Abs(diff)}");
         }
     }
 }
