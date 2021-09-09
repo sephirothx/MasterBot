ddb72829b1acbaebc37ae0f7fec8f891f523ec20
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-09 19:28:21 +0200

    Added OwnerModule

---
+++ MasterBot.Service/Modules/OwnerModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace MasterBot.Service.Modules
+{
+    [Name("Owner")]
+    [Summary("Owner specific commands")]
+    [RequireOwner]
+    public class OwnerModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("setstatus")]
+        [Summary("Sets the status of the bot.")]
+        public async Task SetStatus(ActivityType activity, [Remainder] string text)
+        {
+            await Context.Client.SetGameAsync(text, type: activity);
+        }
+
+        [Command("removestatus")]
+        [Summary("Removes the status of the bot.")]
+        public async Task RemoveStatus()
+        {
+            await Context.Client.SetActivityAsync(new Game("", ActivityType.CustomStatus));
+        }
+    }
+}
