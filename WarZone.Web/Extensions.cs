13f0767abc6dc0b4b879c341c5fddc48d412a508
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-11 02:05:50 +0200

    Added library to call the WarZone Api

---
+++ WarZone.Web/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace WarZone.Web
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddAPI<T>(this IServiceCollection services, string uri) where T : class
+        {
+            services.AddRefitClient<T>().ConfigureHttpClient(c => c.BaseAddress = new Uri(uri));
+
+            return services;
+        }
+    }
+}
