c8b3d84bdbea993a58716a27a44a168d3ae0fb81
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-18 16:38:21 +0200

    Added !clanwars command

--- MasterBot.Service/Modules/ClanWarsModule.cs
+++ MasterBot.Service/Modules/ClanWarsModule.cs
@@ -23,23 +23,21 @@ namespace MasterBot.Service.Modules
         [Summary("Posts an explanation about free wins in Clan War.")]
         public async Task FreeWin()
         {
-            await ReplyAsync(@"Wait Private you don't know what a free win is and how to get it?
-Then give me your ATTENTION and listen up while i explain.
-
-You can only get a free win if all of these requirements are satisfied:
-    1. No other player (or team) from your clan got a game on this template
-    2. No opponents could be found for you (or your team)");
+            await ReplyAsync(Strings.FREE_WIN);
         }
 
         [Command("swarm")]
         [Summary("Posts an explanation about swarming templates in Clan War.")]
         public async Task Swarm()
         {
-            await ReplyAsync(@"Swarming happens when multiple Masters join the same template.
-It has several benefits:
-    1. It ensures that, if some M'Hunters are queued, we're not just filtering the hardest opponents from them
-    2. It makes us get direct matchups against M'Hunters
-    3. It gives us a chance of getting 'easier' opponents on average");
+            await ReplyAsync(Strings.SWARM);
+        }
+
+        [Command("clanwars"), Alias("clanwar", "cw")]
+        [Summary("")]
+        public async Task ClanWarsInfo()
+        {
+            await ReplyAsync(Strings.CLAN_WAR);
         }
 
         [Command("timeslot"), Alias("lastslot", "ts", "ls")]
