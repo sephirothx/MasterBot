51013ed5d691d947d3fec9b611e537ef9e00ba59
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-18 16:37:46 +0200

    Added Strings class

---
+++ MasterBot.Service/Common/Strings.cs
@@ -0,0 +1,36 @@
+﻿namespace MasterBot.Service.Common
+{
+    public static class Strings
+    {
+        public const string CLAN_WAR = @"Here you can find some information about clan wars: <https://www.warzone.com/wiki/Clan_Wars>
+
+**Description**
+ - It's a competition in which clans challenge each other in real time games
+ - Each clan can have at most 40 players playing per season
+ - There are 6 timeslots each day: at 0:00, 4:00, 8:00, 12:00, 16:00 and 20:00 UTC
+ - Each player can play at most 1 game/day
+ - A season lasts between one and two months
+ - Games are on average 10-15 minutes long
+
+**Benefits**
+ - Getting 1st place gives the following rewards: 5,000 coins split evenly among all participants, a Rare Artifact and 1 of every Idle Power
+ - You'll get to join our discord and you will be able to communicate with the best clan in clan wars and some of the best players on warlight
+ - Our discord has a custom bot that will send a ping on every timeslot
+
+**Clan Requirements**
+ - We require a minimum participation of 80% for the season, but it is ok to skip one day or two when you're busy or to take a short break with notice";
+
+        public const string FREE_WIN = @"Wait Private you don't know what a free win is and how to get it?
+Then give me your ATTENTION and listen up while i explain.
+
+You can only get a free win if all of these requirements are satisfied:
+    1. No other player (or team) from your clan got a game on this template
+    2. No opponents could be found for you (or your team)";
+
+        public const string SWARM = @"Swarming happens when multiple Masters join the same template.
+It has several benefits:
+    1. It ensures that, if some M'Hunters are queued, we're not just filtering the hardest opponents from them
+    2. It makes us get direct matchups against M'Hunters
+    3. It gives us a chance of getting 'easier' opponents on average";
+    }
+}
