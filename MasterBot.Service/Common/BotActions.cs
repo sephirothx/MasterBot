d94ad0ee3926a1eee02e4232d86bd7d652845ba4
Stefano Prosperi <step.prosperi@gmail.com>
2021-09-30 18:33:04 +0200

    Added BotActions class

---
+++ MasterBot.Service/Common/BotActions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Discord;
+
+namespace MasterBot.Service.Common
+{
+    public class BotActions
+    {
+        public async Task SendDirectMessageAsync(IUser user, string message = null, Embed embed = null)
+        {
+            var channel = await user.GetOrCreateDMChannelAsync();
+
+            await channel.SendMessageAsync(message, embed: embed);
+            await channel.CloseAsync();
+        }
+    }
+}
