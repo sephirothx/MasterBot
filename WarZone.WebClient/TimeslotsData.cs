c5119e0a1d23359155c748ef91771b7876e34106
Stefano Prosperi <step.prosperi@gmail.com>
2021-10-26 21:06:10 +0200

    Removed API and Web project - replaced with WebClient

---
+++ WarZone.WebClient/TimeslotsData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WarZone.WebClient
+{
+    public class TimeslotsData : ITimeslotsData
+    {
+        public List<string> GetTimeslotTemplates(int id)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            using var driver = new ChromeDriver(chromeOptions);
+            driver.Url = $"https://www.warzone.com/Clans/War?ID=6&Timeslot={id}";
+            var elements = driver.FindElements(By.CssSelector("[id^=ujs_TemplateNameLabel][id$=tmp]"));
+            
+            return !elements.Any()
+                       ? new List<string>()
+                       : elements.Select(e => e.Text).ToList();
+        }
+    }
+}
